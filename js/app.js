50603c40902cc0e4b2928fb208e88465b59ca032
jun <juntetsu.1012@gmail.com>
2024-02-03 04:14:52 +0900

    スクロールに応じてvideoのscaleを変更する処理

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 import { lerp } from "./utils.js";
+
+const video = document.querySelector("video");
+const videoSection = document.querySelector("#video");
+
+window.addEventListener("scroll", () => {
+  // console.log(window.scrollY);
+  animateVideo();
+});
+
+function animateVideo() {
+  let { bottom } = videoSection.getBoundingClientRect();
+  let scale = 1 - (bottom - window.innerHeight) * 0.0005;
+  scale = scale < 0.2 ? 0.2 : scale > 1 ? 1 : scale;
+  video.style.transform = `scale(${scale})`;
+  console.log(bottom);
+}
