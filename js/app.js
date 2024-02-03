b3d8d93c923b09bc37d5b1574672889ff75c7e79
jun <juntetsu.1012@gmail.com>
2024-02-03 14:02:56 +0900

    修正

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 import { lerp } from "./utils.js";
 
+
+/*================================
+スクロールに応じてvideoのscaleを変更
+================================*/
+const main = document.querySelector("main")
 const video = document.querySelector("video");
 const videoSection = document.querySelector("#video");
 
-window.addEventListener("scroll", () => {
-  // console.log(window.scrollY);
+main.addEventListener("scroll", () => {
   animateVideo();
 });
 
 function animateVideo() {
+  // videoSection要素の境界ボックスを取得し、そのbottomプロパティ（要素の下端の位置）をbottom変数に格納
   let { bottom } = videoSection.getBoundingClientRect();
   let scale = 1 - (bottom - window.innerHeight) * 0.0005;
   scale = scale < 0.2 ? 0.2 : scale > 1 ? 1 : scale;
   video.style.transform = `scale(${scale})`;
-  console.log(bottom);
 }
