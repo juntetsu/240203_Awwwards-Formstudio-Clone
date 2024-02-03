875540497baff7ffbaf478a8a5d48268408640f8
jun <juntetsu.1012@gmail.com>
2024-02-03 14:21:22 +0900

    videoセクションのテキストアニメーション

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 import { lerp } from "./utils.js";
 
-
-/*================================
-スクロールに応じてvideoのscaleを変更
-================================*/
 const main = document.querySelector("main")
 const video = document.querySelector("video");
 const videoSection = document.querySelector("#video");
@@ -12,10 +8,19 @@ main.addEventListener("scroll", () => {
   animateVideo();
 });
 
+const headerLeft = document.querySelector(".video__text-left")
+const headerRight = document.querySelector(".video__text-right")
+
 function animateVideo() {
-  // videoSection要素の境界ボックスを取得し、そのbottomプロパティ（要素の下端の位置）をbottom変数に格納
+  // スクロールに応じてvideoのscaleを変更
   let { bottom } = videoSection.getBoundingClientRect();
   let scale = 1 - (bottom - window.innerHeight) * 0.0005;
   scale = scale < 0.2 ? 0.2 : scale > 1 ? 1 : scale;
   video.style.transform = `scale(${scale})`;
+
+  // Text transformation
+  let textTrans = bottom - window.innerHeight;
+  textTrans = textTrans < 0 ? 0 : textTrans;
+  headerLeft.style.transform = `translateX(${-textTrans}px)`;
+  headerRight.style.transform = `translateX(${textTrans}px)`;
 }
