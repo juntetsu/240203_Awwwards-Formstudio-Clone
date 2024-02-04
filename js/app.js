fe508d8597083ddb1ce2123a8233d677591f8d54
jun <juntetsu.1012@gmail.com>
2024-02-05 07:13:11 +0900

    Text Animation & Video & Footer

--- js/app.js
+++ js/app.js
@@ -118,6 +118,39 @@ const scrollDiscover = () => {
   rightText.style.transform = `translateX(${textTrans}px)`;
 };
 
+// Text Reveal
+const textReveals = [...document.querySelectorAll(".text__reveal")];
+
+let callback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry);
+      [...entry.target.querySelectorAll("span")].forEach((span, idx) => {
+        setTimeout(() => {
+          span.style.transform = `translateY(0)`;
+        }, (idx + 1) * 50);
+      });
+    }
+  });
+};
+
+let options = {
+  rootMargin: "0px",
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+textReveals.forEach((text) => {
+  let string = text.innerText;
+  let html = "";
+  for (let i = 0; i < string.length; i++) {
+    html += `<span>${string[i]}</span>`;
+  }
+  text.innerHTML = html;
+  observer.observe(text);
+});
+
 function animate() {
   animateProjects();
   requestAnimationFrame(animate);
